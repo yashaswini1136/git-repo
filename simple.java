2f306f749c3eae1bb829d2c52cea3a6c03ca69d9
yashaswini1136 <yashudad41@gmail.com>
2025-08-19 12:17:13 +0530

    line is added

--- simple.java
+++ simple.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 System.out.println("Welcome to Java Programming!"); 
+System.out.println("Welcome to Java Programming2!"); 
+
+
+
     }
 }
