30fc28ff645ccc689f9e89ac268b46f21613eb2e
yashaswini1136 <yashudad41@gmail.com>
2025-09-23 10:17:36 +0530

    Update StringExample.java

--- StringExample.java
+++ StringExample.java
@@ -4,11 +4,12 @@ public class StringExample {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Create Scanner object
         
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a string: hello world");
         String input = scanner.nextLine(); // Read input from user
         
-        System.out.println("You entered: " + input); // Display the entered string
+        System.out.println("You entered: hello world" + input); // Display the entered string
         
         scanner.close(); // Close the scanner
     }
 }
+
