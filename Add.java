ffc3ec0401e8db136e5ee305f5dfb6d409968733
yashaswini1136 <yashudad41@gmail.com>
2025-11-05 21:55:43 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
